bb83e0b96bc1e3925a2956803a7f0a8d975f6364
kerrymdiving <kerry.murphy@mac.com>
2022-03-03 14:29:46 +0000

    2.2 commit

--- Airport.java/src/TestPassenger.java
+++ Airport.java/src/TestPassenger.java
@@ -1,4 +1,4 @@
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestPassenger {
@@ -6,5 +6,33 @@ public class TestPassenger {
     public void passengerHasAName() {
        Passenger passenger = new Passenger("Kerry", "A123456", "A5");
        assertEquals(passenger.getName() + passenger.getPassportNumber() + passenger.getSeatNumber(), "Kerry" + "A123456" + "A5");
+    }
+
+
+    @Test
+    public void APassengerCanHaveABag() {
+        Passenger passenger = new Passenger("Kerry", "A123456", "A5");
+        Bag bag = new Bag(17);
+        passenger.setBag(bag);
+        assertSame(passenger.getBag(), bag);
+    }
+
+    @Test
+    public void APassengerChecksInBag() {
+        Passenger passenger = new Passenger("Kerry", "A123456", "A5");
+        Bag bag = new Bag(17);
+        passenger.setBag(bag);
+        passenger.getBag();
+        assertSame(passenger.getBag(), null);
+    }
+
+    @Test
+    public void aPassengerCanHaveABoardingPass() {
+        Passenger passenger = new Passenger("Kerry", "A123456", "A5");
+        BoardingPass pass = new BoardingPass(1, 0);
+        assertNull(passenger.getBoardingPass());
+        passenger.setBoardingPass(pass);
+        assertSame(passenger.getBoardingPass(), pass);
+
     }
 }
